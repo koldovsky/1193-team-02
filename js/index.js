db9d2d046966d6457c3aa57b2f17ae0843e8112e
KaterynaManokha <160152430+KaterynaManokha@users.noreply.github.com>
2024-05-28 21:15:57 +0000

    our team js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function init() {
     import('./header-burger-menu.js')
     import('./cottages-carousel.js')
+    import('./cottages-our-team.js')
 }
 
 const totalPartials = document.querySelectorAll('[hx-trigger="load"], [data-hx-trigger="load"]').length;
