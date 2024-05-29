895e9db673133b5255a9db978e857966f99dc50a
Anton Nadieiko <114859342+forcezk1y@users.noreply.github.com>
2024-05-29 20:03:11 +0000

    Consultation form block finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 function init() {
     import('./header-burger-menu.js');
-    import('./consultation-form-send-button.js');
-    import('./consultation-form-input-filter');
+    import('./consultation-form-modal-window.js');
     import('./header-burger-menu.js')
     import('./cottages-carousel.js')
     import('./cottages-our-team.js')
