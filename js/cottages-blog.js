f4aa237e8662325a543d1d3d0421689ea6041850
YaroslavLapshin <84934307+YaroslavLapshin@users.noreply.github.com>
2024-05-29 09:59:50 +0000

    update blog js file

--- js/cottages-blog.js
+++ js/cottages-blog.js
@@ -25,17 +25,24 @@ function renderBlogItems(blogItems) {
     let blogItemHtml = '';
     for (const blogItem of blogItems) {
         blogItemHtml += `<div class="blog__item">
-                <img class="blog__item-img" src="${blogItem.image}" alt="${blogItem.title}">
-                <div class="blog__item-hashtag">${blogItem.hashtag}</div>
-                <h3 class="blog__item-title">${blogItem.title}</h3>
-                <p class="blog__item-text">${blogItem.description}</p>
-                <p class="blog__item-time"><time datetime="2022-06-02">June 02, 2022</time> | Sam Smith</p>
-                <a class="blog__item-button" href="#">Read more <img class="blog__item-button-arrow" src="img/blog/blog-arrow.svg" alt="Arrow"></a>
-            </div>
-        `;
+            <img class="blog__item-img" src="${blogItem.image}" alt="${blogItem.title}">
+            <div class="blog__item-hashtag">${blogItem.hashtag}</div>
+            <h3 class="blog__item-title">${blogItem.title}</h3>
+            <p class="blog__item-text">${blogItem.description}</p>
+            <p class="blog__item-time"><time datetime="2022-06-02">June 02, 2022</time> | Sam Smith</p>
+            <a class="blog__item-button" href="#">Read more <img class="blog__item-button-arrow" src="img/blog/blog-arrow.svg" alt="Arrow"></a>
+        </div>`;
     }
     const blogItemsContainer = document.querySelector('.blog__items');
     blogItemsContainer.innerHTML = blogItemHtml;
-
 }
+
+function removeImgFromFirst() {
+    var firstImage = document.querySelector('.blog__item-img');
+    if (firstImage) {
+        firstImage.remove("img");
+    }
+}
+
 renderBlogItems(blogItemsData);
+removeImgFromFirst();
