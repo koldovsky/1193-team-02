d37a80052874332aff29343e0320f20e0eb96580
oksana0145 <168598890+oksana0145@users.noreply.github.com>
2024-06-04 00:04:56 +0000

    modal window for header hero

--- js/header-burger-menu.js
+++ js/header-burger-menu.js
@@ -2,3 +2,10 @@ document.querySelector('.burger-menu').addEventListener('click', function () {
     this.classList.toggle('active');
     document.querySelector('.burger-menu-information').classList.toggle('open');
 })
+
+document.querySelectorAll('.burger-menu-information a').forEach(function (link) {
+    link.addEventListener('click', function () {
+        document.querySelector('.burger-menu').classList.remove('active');
+        document.querySelector('.burger-menu-information').classList.remove('open');
+    });
+});
